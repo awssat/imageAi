45daecc48ee24f5a5217f4ceeb39c9ae8071d01f
Bader <phpfalcon@gmail.com>
2019-06-10 14:32:55 +0300

    Hello github

---
+++ src/imageAi.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Awssat\ImageAi;
+
+class imageAi
+{
+
+}
---
+++ tests/imageAiTest.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Awssat\ImageAi\Test;
+
+use PHPUnit\Framework\TestCase;
+
+class imageAiTest extends TestCase
+{
+
+}
